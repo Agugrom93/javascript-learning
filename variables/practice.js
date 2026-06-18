bba3557cdb88a406d041c6cdc65b2ef7b2085bd7
Agugrom93 <youremail@gmail.commorgevans@live.co.uk>
2026-06-18 13:46:47 +0100

    var, let and const

---
+++ variables/practice.js
@@ -0,0 +1,12 @@
+var favoriteFood = 'pizza' // Assigns Pizza to favourite food
+var numOfSlices = 8 // Assigns 8 to numofslices
+console.log(favoriteFood) // Prints Pizza
+console.log(numOfSlices) // prints 8
+
+let Pokemon = 'Charizard' // Assigns 'Charizard' to Pokemon
+console.log(Pokemon) // output: Charizard
+Pokemon = 'Ivysaur' // Changes Pokemon to 'Ivysaur'
+console.log(Pokemon) // output: Ivysaur
+
+const myName = "Morgan" // assigns Morgan to myName. Const means it can't be reassigned.
+console.log(myName) // output: Morgan
