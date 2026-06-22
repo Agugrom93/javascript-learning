17ef615b72ea3ccd2d636de402c93e11be848f78
Agugrom93 <morgevans@live.co.uk>
2026-06-22 11:05:15 +0100

    assignment operators, string interpolation

--- variables/practice.js
+++ variables/practice.js
@@ -10,3 +10,20 @@ console.log(Pokemon) // output: Ivysaur
 
 const myName = "Morgan" // assigns Morgan to myName. Const means it can't be reassigned.
 console.log(myName) // output: Morgan
+
+// new practice session
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5; // increases levelUp by 5
+powerLevel -= 100; // decreases powerLevel by 100
+multiplyMe *= 11; // multiplys multiplyMe by 11
+quarterMe /= 4; // divides quarterMe by 4
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
